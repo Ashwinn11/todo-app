448d58d6a160ad411c7bc285c4d8c6318f6890de
Ashwinn11 <ashwinnanbazhagan@gmail.com>
2024-07-07 14:40:23 +0530

    added event handler

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./Style.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [newItem, setNewItem] = useState("");
+  const [items, setItems] = useState([]);
+
+  const handleAdditem = (event) => {
+    event.preventDefault();
+    setItems((prevItems) => [...prevItems, newItem]);
+    console.log(items);
+    setNewItem("");
+  };
+  const handleDeleteItem = (event) => {
+    event.preventDefault();
+    alert();
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New item</label>
+          <input
+            type="text"
+            id="item"
+            value={newItem}
+            onChange={(name) => setNewItem(name.target.value)}
+          ></input>
+        </div>
+        <button className="btn" type="submit" onClick={handleAdditem}>
+          Add
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </form>
+      <h1 className="header">Todo-Application</h1>
+      <ul className="list">
+        {items.map((item, index) => (
+          <li key="item">
+            <label>
+              {item}
+              <button
+                className="btn btn-danger"
+                type="submit"
+                onClick={handleDeleteItem}
+              >
+                Delete
+              </button>
+            </label>
+          </li>
+        ))}
+      </ul>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
